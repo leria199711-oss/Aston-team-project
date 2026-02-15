3741b4f0db1300a255b9692b82d3d2327209cdfb
calendor <calendor57@gmail.com>
2026-02-15 14:30:40 +0300

    Реализован алгоритм сортировки. Добавлен механизм выбора стратегии путем задания поля ввода

---
+++ src/main/java/Barrel.java
@@ -0,0 +1,11 @@
+public class Barrel {
+    public int size;
+    public String material;
+    public String filling;
+
+    public Barrel(int size, String material, String filling){
+        this.size = size;
+        this.material = material;
+        this.filling = filling;
+    }
+}
---
+++ src/main/java/ShellSortMaterial.java
@@ -0,0 +1,16 @@
+public class ShellSortMaterial implements ShellSortInterface{
+
+    public void shellSort(Barrel[] barrels) {
+        int n = barrels.length;
+        int j;
+        for (int gap = n / 2; gap > 0; gap /= 2){
+            for (int i = gap; i < n; i++) {
+                Barrel temp = barrels[i];
+                for (j = i; j >= gap && barrels[j-gap].material.compareTo(temp.material) > 0; j-= gap) {
+                    barrels[j] = barrels[j - gap];
+                }
+                barrels[j] = temp;
+            }
+        }
+    }
+}
