afcbd5ef585ce0f0fe14ba453d12830b0ed7d7ab
Kokovina Ekaterina <kokovinushka@mail.ru>
2026-02-16 17:33:22 +0500

    BarrelBuilder

---
+++ Barrel.java
@@ -0,0 +1,70 @@
+import java.util.Objects;
+
+public class Barrel {
+    private final double volume;
+    private final String material;
+    private final String storedMaterial;
+    private Barrel(Builder builder) {
+        this.volume = builder.volume;
+        this.material = builder.material;
+        this.storedMaterial = builder.storedMaterial;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public String getStoredMaterial() {
+        return storedMaterial;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Barrel barrel = (Barrel) o;
+        return Double.compare(barrel.volume, volume) == 0 && Objects.equals(material, barrel.material) && Objects.equals(storedMaterial, barrel.storedMaterial);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(volume, material, storedMaterial);
+    }
+
+    @Override
+    public String toString() {
+        return "Barrel{" +
+                "volume=" + volume +
+                ", material='" + material + '\'' +
+                ", storedMaterial='" + storedMaterial + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private double volume;
+        private String material;
+        private String storedMaterial;
+
+        public Builder setVolume(double volume) {
+            this.volume = volume;
+            return this;
+        }
+        public Builder setMaterial(String material) {
+            this.material = material;
+            return this;
+        }
+        public Builder setStoredMaterial(String storedMaterial) {
+            this.storedMaterial = storedMaterial;
+            return this;
+        }
+        public Barrel build(){
+            return new Barrel(this);
+        }
+
+    }
+
+}
