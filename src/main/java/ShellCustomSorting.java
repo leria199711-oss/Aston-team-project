98f4469d59f8de3c84a61b2a21c299280eaf3b14
calendor <calendor57@gmail.com>
2026-02-24 22:09:44 +0300

    При более детальном просмотре обнаружил ошибки в алгоритме. Откат к стабильной версии через доп массивы.

--- src/main/java/ShellCustomSorting.java
+++ src/main/java/ShellCustomSorting.java
@@ -1,39 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShellCustomSorting implements ShellSortInterface{
     public void shellSort(Barrel[] barrels){
-        for (int gap = barrels.length / 2; gap > 0; gap /= 2) {
-            for (int i = gap; i < barrels.length; i++){
-                if ((int)barrels[i].getVolume() % 2 == 0){
-                    int j = i - gap;
-                    boolean sortedPartFound = false;
-                    while (j >= 0 && !sortedPartFound) {
-                        if ((int) barrels[j].getVolume() % 2 == 0){
-                            if (barrels[j].getVolume() > barrels[j + gap].getVolume()) {
-                                Barrel temp = barrels[j];
-                                barrels[j] = barrels[j + gap];
-                                barrels[j + gap] = temp;
-                                j = j - gap;
-                            } else sortedPartFound = true;
-                        } else {
-                            int k = j - gap;
-                            boolean evenNumberFound = false;
-                            while (k >= 0 && !evenNumberFound) {
-                                if ((int)barrels[k].getVolume() % 2 == 0){
-                                    evenNumberFound = true;
-                                    if (barrels[k].getVolume() > barrels[j+gap].getVolume()){
-                                        Barrel temp = barrels[k];
-                                        barrels[k] = barrels[j + gap];
-                                        barrels[j + gap] = temp;
-                                        j = k - gap;
-                                    }
-                                } else {
-                                    k = k - gap;
-                                }
-                            }
-                            sortedPartFound = true;
-                        }
-                    }
-                }
+        List<Barrel> onlyEvenBarrels = new ArrayList<>();
+        List<Integer> originalIndexes = new ArrayList<>();
+        for (int i = 0; i < barrels.length; i++) {
+            if ((int)barrels[i].getVolume() % 2 == 0) {
+                onlyEvenBarrels.add(barrels[i]);
+                originalIndexes.add(i);
             }
+        }
+        Barrel[] onlyEvenBarrelsArray = new Barrel[onlyEvenBarrels.size()];
+        for (int i = 0; i < onlyEvenBarrelsArray.length ; i++){
+            onlyEvenBarrelsArray[i] = onlyEvenBarrels.get(i);
+        }
+        ShellSortInterface sorter = new ShellSortSize();
+        sorter.shellSort(onlyEvenBarrelsArray);
+        for (int i = 0; i < onlyEvenBarrelsArray.length; i++){
+            barrels[originalIndexes.get(i)] = onlyEvenBarrelsArray[i];
         }
     }
 }
